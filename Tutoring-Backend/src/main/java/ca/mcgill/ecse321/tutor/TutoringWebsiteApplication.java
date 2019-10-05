fef7af21a9b953929f1cddc9b3762bd5ee35f35f
Er Jun Li <er.li@mail.mcgill.ca>
2019-10-05 19:27:03 -0400

    Created project structure, renamed readme file and created Tutoring-Backend Spring Project

---
+++ Tutoring-Backend/src/main/java/ca/mcgill/ecse321/tutor/TutoringWebsiteApplication.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.tutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutoringWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutoringWebsiteApplication.class, args);
+	}
+
+}
