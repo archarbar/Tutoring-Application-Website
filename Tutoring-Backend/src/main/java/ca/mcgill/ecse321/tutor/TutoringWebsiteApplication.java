7ed173d66e763697ca2d8f6a09884d45388cdfed
Er Jun Li <er.li@mail.mcgill.ca>
2019-10-08 14:48:24 -0400

    Testing Hello World

--- Tutoring-Backend/src/main/java/ca/mcgill/ecse321/tutor/TutoringWebsiteApplication.java
+++ Tutoring-Backend/src/main/java/ca/mcgill/ecse321/tutor/TutoringWebsiteApplication.java
@@ -2,12 +2,21 @@ package ca.mcgill.ecse321.tutor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+
+@RestController
 @SpringBootApplication
 public class TutoringWebsiteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TutoringWebsiteApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String greeting(){
+	  return "Hello, world!";
 	}
 
 }
