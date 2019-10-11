9d14239c75cd5d0c43f0ff257f47415ac3b077f3
Victor Zhong <archarbar@hotmail.fr>
2019-10-11 16:04:09 -0400

    correct directory structure and created dao and service classes

---
+++ Tutoring-Backend/src/test/java/ca/mcgill/ecse321/tutor/TestTutoringWebsiteService.java
@@ -0,0 +1,25 @@
+package ca.mcgill.ecse321.tutor;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestTutoringWebsiteService{
+
+	@Test 
+}
