c28c094b58eda4d7c8fdc1c86a97d1645af23b8a
Victor Zhong <archarbar@hotmail.fr>
2019-10-11 18:20:52 -0400

    implemented dao classes for my 4 classes

--- Tutoring-Backend/src/main/java/ca/mcgill/ecse321/tutor/dao/RatingRepository.java
+++ Tutoring-Backend/src/main/java/ca/mcgill/ecse321/tutor/dao/RatingRepository.java
@@ -1,5 +1,13 @@
 package main.java.ca.mcgill.ecse321.tutor.dao;
 
-public class RatingRepository {
+import java.util.List;
+import main.java.ca.mcgill.ecse321.tutor.model.Rating;
+import main.java.ca.mcgill.ecse321.tutor.model.Student;
+import main.java.ca.mcgill.ecse321.tutor.model.Tutor;
+import org.springframework.data.repository.CrudRepository;
 
+public interface RatingRepository extends CrudRepository<Rating, Integer> {
+	Rating findRatingById(Integer Id);
+	List<Rating> findByTutor(Tutor tutor);
+	List<Rating> findByStudent(Student student);
 }
--- Tutoring-Backend/src/main/java/ca/mcgill/ecse321/tutor/dao/RoomRepository.java
+++ Tutoring-Backend/src/main/java/ca/mcgill/ecse321/tutor/dao/RoomRepository.java
@@ -1,5 +1,10 @@
 package main.java.ca.mcgill.ecse321.tutor.dao;
 
-public class RoomRepository {
+import main.java.ca.mcgill.ecse321.tutor.model.Room;
+import main.java.ca.mcgill.ecse321.tutor.model.TutoringSession;
+import org.springframework.data.repository.CrudRepository;
 
+public interface RoomRepository extends CrudRepository<Room, Integer> {
+	Room findRoomById(Integer Id);
+	Room findByTutoringSession(TutoringSession tutoringSession);
 }
