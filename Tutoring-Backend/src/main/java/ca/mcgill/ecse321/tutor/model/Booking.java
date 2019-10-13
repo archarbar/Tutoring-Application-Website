45cf3a921966ef3f33738e69f5dc83e7043c46da
Tony Ou <tonyou1999@hotmail.com>
2019-10-13 15:40:25 -0400

    Added manually missing methods in Booking and TimeSlot

--- Tutoring-Backend/src/main/java/ca/mcgill/ecse321/tutor/model/Booking.java
+++ Tutoring-Backend/src/main/java/ca/mcgill/ecse321/tutor/model/Booking.java
@@ -1,6 +1,7 @@
 package ca.mcgill.ecse321.tutor.model;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import java.util.Set;
@@ -10,86 +11,96 @@ import java.sql.Date;
 
 @Entity
 public class Booking{
-private Course course;
+	private Course course;
 
-@ManyToOne(optional=false)
-public Course getCourse() {
-   return this.course;
-}
+	@ManyToOne(optional=false)
+	public Course getCourse() {
+		return this.course;
+	}
 
-public void setCourse(Course course) {
-   this.course = course;
-}
+	public void setCourse(Course course) {
+		this.course = course;
+	}
 
-private String tutorEmail;
+	private String tutorEmail;
 
-public void setTutorEmail(String value) {
-   this.tutorEmail = value;
-}
+	public void setTutorEmail(String value) {
+		this.tutorEmail = value;
+	}
 
-public String getTutorEmail() {
-   return this.tutorEmail;
-}
+	public String getTutorEmail() {
+		return this.tutorEmail;
+	}
 
-/**
- * <pre>
- *           1..1     1..1
- * Booking ------------------------> Date
- *           &lt;       specificDate
- * </pre>
- */
-private Date specificDate;
+	/**
+	 * <pre>
+	 *           1..1     1..1
+	 * Booking ------------------------> Date
+	 *           &lt;       specificDate
+	 * </pre>
+	 */
+	private Date specificDate;
 
-public void setSpecificDate(Date value) {
-   this.specificDate = value;
-}
+	public void setSpecificDate(Date value) {
+		this.specificDate = value;
+	}
 
-public Date getSpecificDate() {
-   return this.specificDate;
-}
+	public Date getSpecificDate() {
+		return this.specificDate;
+	}
 
-private TimeSlot timeSlot;
+	private TimeSlot timeSlot;
 
-@OneToOne(optional=false)
-public TimeSlot getTimeSlot() {
-   return this.timeSlot;
-}
+	@OneToOne(optional=false)
+	public TimeSlot getTimeSlot() {
+		return this.timeSlot;
+	}
 
-public void setTimeSlot(TimeSlot timeSlot) {
-   this.timeSlot = timeSlot;
-}
+	public void setTimeSlot(TimeSlot timeSlot) {
+		this.timeSlot = timeSlot;
+	}
 
-private Notification notification;
+	private Notification notification;
 
-@OneToOne(mappedBy="booking", optional=false)
-public Notification getNotification() {
-   return this.notification;
-}
+	@OneToOne(mappedBy="booking", optional=false)
+	public Notification getNotification() {
+		return this.notification;
+	}
 
-public void setNotification(Notification notification) {
-   this.notification = notification;
-}
+	public void setNotification(Notification notification) {
+		this.notification = notification;
+	}
 
-private Set<Student> student;
+	private Set<Student> student;
 
-@ManyToMany
-public Set<Student> getStudent() {
-   return this.student;
-}
+	@ManyToMany
+	public Set<Student> getStudent() {
+		return this.student;
+	}
 
-public void setStudent(Set<Student> students) {
-   this.student = students;
-}
+	public void setStudent(Set<Student> students) {
+		this.student = students;
+	}
 
-private TutoringSession tutoringSession;
+	private TutoringSession tutoringSession;
 
-@OneToOne(mappedBy="booking", optional=false)
-public TutoringSession getTutoringSession() {
-   return this.tutoringSession;
-}
+	@OneToOne(mappedBy="booking", optional=false)
+	public TutoringSession getTutoringSession() {
+		return this.tutoringSession;
+	}
 
-public void setTutoringSession(TutoringSession tutoringSession) {
-   this.tutoringSession = tutoringSession;
-}
+	public void setTutoringSession(TutoringSession tutoringSession) {
+		this.tutoringSession = tutoringSession;
+	}
+
+	private Integer bookingId;
+
+	public void setBookingId(Integer value) {
+		this.bookingId = value;
+	}
+	@Id
+	public Integer getBookingId() {
+		return this.bookingId;
+	}
 
 }
--- Tutoring-Backend/src/main/java/ca/mcgill/ecse321/tutor/model/TimeSlot.java
+++ Tutoring-Backend/src/main/java/ca/mcgill/ecse321/tutor/model/TimeSlot.java
@@ -3,49 +3,73 @@ package ca.mcgill.ecse321.tutor.model;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+
+import java.sql.Date;
 import java.util.Set;
 import javax.persistence.OneToMany;
 
+import java.sql.Time;
+
 @Entity
 public class TimeSlot{
-private Integer timeSlotId;
-   
-   public void setTimeSlotId(Integer value) {
-this.timeSlotId = value;
-    }
-@Id
-public Integer getTimeSlotId() {
-return this.timeSlotId;
-    }
-private Tutor tutor;
+	private Integer timeSlotId;
 
-@ManyToOne(optional=false)
-public Tutor getTutor() {
-   return this.tutor;
-}
-
-public void setTutor(Tutor tutor) {
-   this.tutor = tutor;
-}
-
-private Set<TutoringSession> tutoringSession;
-
-@OneToMany(mappedBy="timeSlot")
-public Set<TutoringSession> getTutoringSession() {
-   return this.tutoringSession;
-}
-
-public void setTutoringSession(Set<TutoringSession> tutoringSessions) {
-   this.tutoringSession = tutoringSessions;
-}
-
-private DayOfTheWeek dayOfTheWeek;
-
-public void setDayOfTheWeek(DayOfTheWeek dayOfTheWeek) {
-  this.dayOfTheWeek = dayOfTheWeek;
-}
-
-public DayOfTheWeek getDayOfTheWeek() {
-   return this.dayOfTheWeek;
-}
+	public void setTimeSlotId(Integer value) {
+		this.timeSlotId = value;
 	}
+	@Id
+	public Integer getTimeSlotId() {
+		return this.timeSlotId;
+	}
+	private Tutor tutor;
+
+	@ManyToOne(optional=false)
+	public Tutor getTutor() {
+		return this.tutor;
+	}
+
+	public void setTutor(Tutor tutor) {
+		this.tutor = tutor;
+	}
+
+	private Set<TutoringSession> tutoringSession;
+
+	@OneToMany(mappedBy="timeSlot")
+	public Set<TutoringSession> getTutoringSession() {
+		return this.tutoringSession;
+	}
+
+	public void setTutoringSession(Set<TutoringSession> tutoringSessions) {
+		this.tutoringSession = tutoringSessions;
+	}
+
+	private DayOfTheWeek dayOfTheWeek;
+
+	public void setDayOfTheWeek(DayOfTheWeek dayOfTheWeek) {
+		this.dayOfTheWeek = dayOfTheWeek;
+	}
+
+	public DayOfTheWeek getDayOfTheWeek() {
+		return this.dayOfTheWeek;
+	}
+
+	private Time startTime;
+
+	public void setStartTime(Time value) {
+		this.startTime = value;
+	}
+
+	public Time getStartTime() {
+		return this.startTime;
+	}
+
+	private Time endtime;
+
+	public void setEndTime(Time value) {
+		this.endtime = value;
+	}
+
+	public Time getEndTime() {
+		return this.endtime;
+	}
+}
