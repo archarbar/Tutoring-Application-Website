9d7f2dcd2b20a7700fa59901f233110ae82d3157
Er Jun Li <er.li@mail.mcgill.ca>
2019-10-13 20:28:50 -0400

    Written test for Studend

---
+++ Tutoring-Backend/src/test/java/ca/mcgill/ecse321/tutor/TimeSlotTests.java
@@ -0,0 +1,19 @@
+package ca.mcgill.ecse321.tutor;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TimeSlotTests {
+
+	@Test
+	public void testCreateTimeSlot() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTimeSlot() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Tutoring-Backend/src/test/java/ca/mcgill/ecse321/tutor/TutoringSessionTests.java
@@ -0,0 +1,19 @@
+package ca.mcgill.ecse321.tutor;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TutoringSessionTests {
+
+	@Test
+	public void testCreateTutoringSession() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTutoringSession() {
+		fail("Not yet implemented");
+	}
+
+}
