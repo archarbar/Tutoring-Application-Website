eea81c539a12f80d2eb6491d7dc5eb4ac4e4465c
Victor Zhong <archarbar@hotmail.fr>
2019-10-14 22:29:58 -0400

    added constructor tests

--- Tutoring-Backend/src/test/java/ca/mcgill/ecse321/tutor/service/TestTutoringService.java
+++ Tutoring-Backend/src/test/java/ca/mcgill/ecse321/tutor/service/TestTutoringService.java
@@ -19,7 +19,8 @@ import ca.mcgill.ecse321.tutor.dao.*;
 import ca.mcgill.ecse321.tutor.model.*;
 import ca.mcgill.ecse321.tutor.service.*;
 
-
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class TestTutoringService {
 
 	@Autowired
@@ -80,7 +81,6 @@ public class TestTutoringService {
 		notificationRepository.deleteAll();
 		ratingRepository.deleteAll();
 		timeslotRepository.deleteAll();
-
 	}
 	
 	/*
@@ -210,7 +210,6 @@ public class TestTutoringService {
 		assertEquals(tutor.getUserId(), allTutors.get(0).getUserId());
 	}
 	
-	
 	/*
 	 * BOOKING TESTS
 	 */
@@ -273,7 +272,92 @@ public class TestTutoringService {
 		assertEquals(booking.getId(), allBookings.get(0).getId());
 	}
 	
+	/*
+	 *  COURSE TESTS
+	 */
 	
+	@Test
+	public void testCreateCourse() { // test constructor method
+		assertEquals(0, courseService.getAllCourses().size());
+		String name = "English";
+		Level level = Level.UNIVERSITY;
+		try {
+			courseService.createCourse(name, level);
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
+		List<Course> allCourses = courseService.getAllCourses();
+		assertEquals(1, allCourses.size());
+		assertEquals("English", allCourses.get(0).getCourseName());
+		assertEquals(Level.UNIVERSITY, allCourses.get(0).getCourseLevel());
+	}
+	
+	/*
+	 *  ROOM TESTS
+	 */
+	
+	@Test
+	public void testCreateRoom() { // test constructor method
+		assertEquals(0, roomService.getAllRooms().size());
+		Integer number = 12;
+		Integer capacity = 30;
+		try {
+			roomService.createRoom(number, capacity);
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
+		List<Room> allRooms = roomService.getAllRooms();
+		assertEquals(1, allRooms.size());
+		assertEquals((Integer) 12, allRooms.get(0).getRoomNumber());
+		assertEquals((Integer) 30, allRooms.get(0).getRoomCapacity());
+	}
+	
+	/*
+	 *  NOTIFICATION TESTS
+	 */
+	
+	@Test
+	public void testCreateNotification() { // test constructor method
+		assertEquals(0, notificationService.getAllNotifications().size());
+		String firstName = "Arthur";
+		String lastName = "Morgan";
+		String tutorEmail = "arthurmorgan@redemption.com";
+		String password = "tahiti";
+		Tutor tutor = tutorService.createTutor(firstName, lastName, tutorEmail, password);
+		String studentEmail = "johnmarston@redemption.com";
+		Course course = courseService.createCourse("test", Level.CEGEP);
+		TimeSlot timeSlot = timeSlotService.createTimeSlot(Time.valueOf("10:12:12"), Time.valueOf("12:12:12"), DayOfTheWeek.THURSDAY);
+		Booking booking = bookingService.createBooking(tutorEmail, studentEmail, Date.valueOf("2019-10-10"), timeSlot, course);
+		try {
+			notificationService.createNotification(booking, tutorEmail);
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
+		List<Notification> allNotifications = notificationService.getAllNotifications();
+		assertEquals(1, allNotifications.size());
+		assertEquals(booking, allNotifications.get(0).getBooking());
+		assertEquals(tutor, allNotifications.get(0).getTutor());
+	}
+	
+	/*
+	 *  RATING TESTS
+	 */
+	
+	@Test
+	public void testCreateRating() { // test constructor method
+		assertEquals(0, ratingService.getAllRatings().size());
+		Integer stars = 4;
+		String comment = "Best tutor!";
+		try {
+			ratingService.createRating(stars, comment);
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
+		List<Rating> allRatings = ratingService.getAllRatings();
+		assertEquals(1, allRatings.size());
+		assertEquals((Integer) 12, allRatings.get(0).getStars());
+		assertEquals((Integer) 30, allRatings.get(0).getComment());
+	}
 	
 
 }
