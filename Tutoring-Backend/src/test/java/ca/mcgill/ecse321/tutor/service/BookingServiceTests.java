02037a992381f77113053e0bd7ef6341415e4ddf
Tony Ou <tonyou1999@hotmail.com>
2019-10-24 13:57:49 -0400

    Finished tests for course service

--- Tutoring-Backend/src/test/java/ca/mcgill/ecse321/tutor/service/BookingServiceTests.java
+++ Tutoring-Backend/src/test/java/ca/mcgill/ecse321/tutor/service/BookingServiceTests.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.*;
 
 import java.sql.Date;
 import java.sql.Time;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
