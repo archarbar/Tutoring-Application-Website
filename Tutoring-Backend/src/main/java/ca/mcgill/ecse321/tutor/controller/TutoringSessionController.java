5f54f468f5cbe5dab6771878b32d2b83409e8094
Tony Ou <tonyou1999@hotmail.com>
2019-10-31 15:08:01 -0400

    Fixed leftover conflicts

--- Tutoring-Backend/src/main/java/ca/mcgill/ecse321/tutor/controller/TutoringSessionController.java
+++ Tutoring-Backend/src/main/java/ca/mcgill/ecse321/tutor/controller/TutoringSessionController.java
@@ -14,48 +14,42 @@ import java.util.List;
 @RestController
 public class TutoringSessionController {
 
-    @Autowired
-    private TutoringSessionService service;
-    
-    @Autowired
-    private BookingRepository bookingRepository;
+	@Autowired
+	private TutoringSessionService service;
 
-    @PostMapping("/tutoringSession/new")
-<<<<<<< HEAD
-    // Need to figure out how we get a room and assign it
-    public TutoringSessionDto createTutoringSession (@RequestParam Notification notification, @RequestParam Room room){
-        TutoringSession tutoringSession = service.createTutoringSession(notification.getBooking().getSpecificDate(),
-                notification.getTutor(), room, notification.getBooking(), notification.getBooking().getTimeSlot());
-=======
-    public TutoringSessionDto createTutoringSession(
-    												@RequestParam("bookingId") String bookingId,
-    												@RequestParam("booking2") Booking booking2,
-    												@RequestParam("room") Room room, 
-    												@RequestParam("tutor") Tutor tutor){
-        Booking booking = bookingRepository.findBookingById(Integer.parseInt(bookingId));
-    	TutoringSession tutoringSession = service.createTutoringSession(booking.getSpecificDate(),
-                tutor, room, booking, booking.getTimeSlot());
->>>>>>> 8fd723fcc1b46d2c79e549b35d2f1482010a9446
-        return convertToDto(tutoringSession);
-    }
+	@Autowired
+	private BookingRepository bookingRepository;
 
-    @GetMapping("/tutoringSession/{tutoringSessionId}")
-    public TutoringSessionDto getTutoringSessionById(@PathVariable int tutoringSessionId) {
-        return convertToDto(service.getTutoringSessionById(tutoringSessionId));
-    }
+	@PostMapping("/tutoringSession/new")
 
-    @GetMapping("/tutoringSession/tutor/{tutor}")
-    public List<TutoringSessionDto> getTutoringSessionByTutor(@PathVariable Tutor tutor) {
-        List<TutoringSessionDto> tutoringSessionDtos = new ArrayList<>();
-        for (TutoringSession tutoringSession : service.getTutoringSessionByTutor(tutor)) {
-            tutoringSessionDtos.add(convertToDto(tutoringSession));
-        }
-        return tutoringSessionDtos;
-    }
+	public TutoringSessionDto createTutoringSession(
+			@RequestParam("bookingId") String bookingId,
+			@RequestParam("booking2") Booking booking2,
+			@RequestParam("room") Room room, 
+			@RequestParam("tutor") Tutor tutor){
+		Booking booking = bookingRepository.findBookingById(Integer.parseInt(bookingId));
+		TutoringSession tutoringSession = service.createTutoringSession(booking.getSpecificDate(),
+				tutor, room, booking, booking.getTimeSlot());
+		return convertToDto(tutoringSession);
+	}
 
-    private TutoringSessionDto convertToDto(TutoringSession tutoringSession) {
-        if (tutoringSession == null) throw new IllegalArgumentException("This tutoringSession does not exist!");
-        return new TutoringSessionDto(tutoringSession.getSessionDate(), tutoringSession.getTutor(), tutoringSession.getRoom(),
-                tutoringSession.getTimeSlot(), tutoringSession.getBooking(), tutoringSession.getId());
-    }
+	@GetMapping("/tutoringSession/{tutoringSessionId}")
+	public TutoringSessionDto getTutoringSessionById(@PathVariable int tutoringSessionId) {
+		return convertToDto(service.getTutoringSessionById(tutoringSessionId));
+	}
+
+	@GetMapping("/tutoringSession/tutor/{tutor}")
+	public List<TutoringSessionDto> getTutoringSessionByTutor(@PathVariable Tutor tutor) {
+		List<TutoringSessionDto> tutoringSessionDtos = new ArrayList<>();
+		for (TutoringSession tutoringSession : service.getTutoringSessionByTutor(tutor)) {
+			tutoringSessionDtos.add(convertToDto(tutoringSession));
+		}
+		return tutoringSessionDtos;
+	}
+
+	private TutoringSessionDto convertToDto(TutoringSession tutoringSession) {
+		if (tutoringSession == null) throw new IllegalArgumentException("This tutoringSession does not exist!");
+		return new TutoringSessionDto(tutoringSession.getSessionDate(), tutoringSession.getTutor(), tutoringSession.getRoom(),
+				tutoringSession.getTimeSlot(), tutoringSession.getBooking(), tutoringSession.getId());
+	}
 }
