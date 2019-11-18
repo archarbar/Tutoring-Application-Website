77c9ebb003f9dedbff29f0c4ce4bd1f4ccb29d56
Victor Zhong <archarbar@hotmail.fr>
2019-11-17 23:21:54 -0500

    fixed delete timeslot

--- Tutoring-Backend/src/main/java/ca/mcgill/ecse321/tutor/service/TutorService.java
+++ Tutoring-Backend/src/main/java/ca/mcgill/ecse321/tutor/service/TutorService.java
@@ -149,6 +149,10 @@ public class TutorService {
 		if (error.length() > 0) {
 			throw new IllegalArgumentException(error);
 		}
+		TimeSlot timeslot = timeSlotRepository.findTimeSlotById(timeSlotId);
+		Set<TimeSlot> timeslots = tutor.getTimeSlot();
+		timeslots.remove(timeslot);
+		tutor.setTimeSlot(timeslots);
 		timeSlotRepository.deleteById(timeSlotId);
 	}
 
